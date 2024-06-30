5d696ff0badcd6936ffeb53300840161b2e779b9
elesawy <elesawy928@gmail.com>
2024-06-30 09:19:41 +0300

    update image of splash native screen

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -8,5 +8,9 @@ class Assets {
   /// Assets for assetsMixFruit
   /// assets/mix_fruit.png
   static const String assetsMixFruit = "assets/mix_fruit.png";
+
+  /// Assets for assetsMixFruitPng11
+  /// assets/mix_fruit_png_11.png
+  static const String assetsMixFruitPng11 = "assets/mix_fruit_png_11.png";
 }
 
