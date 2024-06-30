ce5a461728cd61fb77d76644ead4dd9706f95a2f
elesawy <elesawy928@gmail.com>
2024-06-30 08:38:49 +0300

    basic code

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,12 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsFruitMarket
+  /// assets/fruit_market.png
+  static const String assetsFruitMarket = "assets/fruit_market.png";
+
+  /// Assets for assetsMixFruit
+  /// assets/mix_fruit.png
+  static const String assetsMixFruit = "assets/mix_fruit.png";
+}
+
